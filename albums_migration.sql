0799f5de0802cf2b315e8cd3879934b207e72bf6
laroblesaguilar <laroblesaguilar@gmail.com>
2019-04-08 16:12:43 -0500

    completed exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(100),
+name VARCHAR(100),
+release_date INT,
+sales DECIMAL(9,2),
+genre VARCHAR(100),
+ PRIMARY KEY (id)
+);
